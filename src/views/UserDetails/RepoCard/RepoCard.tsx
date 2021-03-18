d4fa70323f619f29a867672fad5e2bd6eb3316a8
Nathan Chacon <nathanchacon321@gmail.com>
2021-03-18 00:53:29 -0300

    Add repo cards

---
+++ src/views/UserDetails/RepoCard/RepoCard.tsx
@@ -0,0 +1,21 @@
+import './RepoCard.css'
+function RepoCard(props:RepoCardProps){
+    return (
+        <div className="card bg-secondary">
+            <h1>
+                <a href={props.link} target="blank">{props.title}</a>
+            </h1>
+            <p>{props.description}</p>
+        </div>
+    )
+}
+
+interface RepoCardProps{
+    title:string,
+    description:string
+    link:string,
+    date:string,
+    private:boolean
+}
+
+export default RepoCard
--- src/views/UserDetails/UserDetais.tsx
+++ src/views/UserDetails/UserDetais.tsx
@@ -1,19 +1,23 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router"
 import axios from '../../utils/api'
+import RepoCard from "./RepoCard/RepoCard"
 import './UserDetails.css'
 function UserDetails(){
     const {userName} = useParams<{userName:string}>()
     const [user, setUser] = useState<User>()
+    const [userRepos, setUserRepos] = useState<Array<Repo>>([])
     const [error, setError] = useState<string>('')
     useEffect(() => {
         startUserConfig(userName)
-    }, [])
+    },[])
 
     const startUserConfig = async (userName:string) => {
         try{
             const user = await getUserByName(userName)
+            const userRepos = await getReposByUserName(userName)
             setUser(user)
+            setUserRepos([...userRepos])
         }
         catch(error){
             setError(error)
@@ -33,11 +37,42 @@ function UserDetails(){
                 }
             )
         })
+    }
 
+    const getReposByUserName = (userName:string):Promise<Array<Repo>> => {
+        return new Promise((resolve, reject) =>  {
+            axios.get(`${userName}/repos`)
+            .then(
+                (response) => {
+                    console.log(response)
+                    response.data ? resolve(response.data) : reject('User not found')
+                },
+                (error) => {
+                    console.log(error)
+                    reject('User not found')
+                }
+            )
+        })
+    }
+
+    const getStarredReposByUserName = (userName:string) => {
+        return new Promise((resolve, reject) =>  {
+            axios.get(`${userName}/starred`)
+            .then(
+                (response) => {
+                    console.log(response)
+                    response.data ? resolve(response.data) : reject('User not found')
+                },
+                (error) => {
+                    console.log(error)
+                    reject('User not found')
+                }
+            )
+        })
     }
 
     return (
-        <section className="container bg-primary text-primary d-flex a-center j-center"> 
+        <section className="container bg-primary text-primary d-flex col p-20"> 
             {
                 user
                 ?
@@ -45,14 +80,23 @@ function UserDetails(){
                     <figure className="user-details__avatar">
                         <img src={user.avatar_url}></img>
                     </figure>
-                    <h2 className="user-details__title">Name: {user.login}</h2>
-                    {user.email ? <h4 className="user-details__subtitle">Email: {user.email}</h4> : ''}
-                    {user.followers ? <h4 className="user-details__subtitle">Fallowers: {user.followers}</h4> : ''}
-                    <button className="button button__theme__primary p-10">repositories</button>
+                    <div className="user-details__text-container">
+                        <h2 className="user-details__title">Name: {user.login}</h2>
+                        {user.email ? <h4 className="user-details__subtitle">Email: {user.email}</h4> : ''}
+                        {user.followers ? <h4 className="user-details__subtitle">Fallowers: {user.followers}</h4> : ''}
+                        {user.bio ? <h4 className="user-details__subtitle">Bio: {user.bio}</h4> : ''}
+                    </div>
                 </div>
                 :
                 ''
             }
+            <div>
+                {
+                    userRepos.map((repo) => {
+                        return <RepoCard title={repo.name} link={repo.clone_url} description={repo.description} private={repo.private} date={repo.updated_at}></RepoCard>
+                    })
+                }
+            </div>
         </section>
     )
 }
@@ -63,6 +107,15 @@ interface User {
     email:string,
     bio:string,
     followers:number
+}
+
+interface Repo{
+    name:string
+    description: string,
+    updated_at:string,
+    clone_url:string,
+    forks:number,
+    private:boolean
 }
 
 
