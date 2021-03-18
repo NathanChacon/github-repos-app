aa4243ae0980639ddef9f90f0763e99848d6a5ef
Nathan Chacon <nathanchacon321@gmail.com>
2021-03-18 01:01:14 -0300

    Redirect to user details when click search

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,8 +1,10 @@
 import './Home.css'
 import SearchInput from '../../components/SearchInput/SearchInput'
+import { useHistory } from 'react-router-dom';
 function Home(){
+    const history = useHistory()
     const onSearch = (value:string) => {
-        console.log(value)
+        history.push(`/user/${value}`)
     }
     return (
         <section className="container bg-primary text-primary d-flex a-center j-center">
