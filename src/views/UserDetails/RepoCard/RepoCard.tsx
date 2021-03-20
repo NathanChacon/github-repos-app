fc34027670b902fc9d69d952c9fa3c1299040136
Nathan Chacon <nathanchacon.lemobs@gmail.com>
2021-03-19 22:00:03 -0300

    Add card footer

--- src/views/UserDetails/RepoCard/RepoCard.tsx
+++ src/views/UserDetails/RepoCard/RepoCard.tsx
@@ -1,21 +1,33 @@
 import './RepoCard.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function RepoCard(props:RepoCardProps){
     return (
         <div className="card text-primary">
-            <h1>
-                <a href={props.link} target="blank">{props.title}</a>
-            </h1>
-            <p>{props.description}</p>
+            <div className="card__main-content">
+                <h1 className="card__header">
+                    <a href={props.link} target="blank">{props.title}</a>
+                </h1>
+                <p className="card__description">{props.description}</p>
+            </div>
+            <div className="card__footer">
+                {
+                    props.footer.map((data) => {
+                        return <h5 className="card__footer-content">
+                                    <FontAwesomeIcon icon={data.icon} className="card__footer-content__icon"></FontAwesomeIcon>
+                                    {data.label}
+                                </h5>
+                    })
+                }
+            </div>
         </div>
     )
 }
 
-interface RepoCardProps{
+export interface RepoCardProps{
     title:string,
     description:string
     link:string,
-    date:string,
-    private:boolean
+    footer:Array<{icon:any, label:string}>
 }
 
 export default RepoCard
